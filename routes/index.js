a2802b5e2324bf94d3d80ffb607bfe7d4f75b2e9
lawrencereyes <lawrencereyes96@gmail.com>
2017-09-01 17:05:05 -0400

    CashPro Website Files

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Home page. */
+/* HOME PAGE */
+// GET Home page.
 router.get('/', function(req, res) {
   res.render('home');
 });
 
-/* GET About page */
+// GET About page 
 router.get('/AboutUs', function(req, res){
 	res.render('aboutus');
 });
 
-/* GET Contact Us page */
+// GET Contact Us page
 router.get('/ContactUs', function(req, res){
 	res.render('contactus');
 });
 
-/* GET Services page */
+// GET Services page
 router.get('/Services', function(req, res){
 	res.render('services');
 });
+/* END HOME PAGE */
 
-/* DROPWDOWN MENU */
-/* GET Accounting page */
-router.get('/Services/Accounting', function(req, res){
-	res.render('services');
+/* SERVICES */
+// GET Accounting page 
+router.get('/Accounting', function(req, res){
+	res.render('accounting');
 });
 
-/* GET Consulting page */
-router.get('/Services/Consulting', function(req, res){
-	res.render('services');
+// GET Consulting page 
+router.get('/Consulting', function(req, res){
+	res.render('consulting');
 });
 
-/* GET Taxes page */
-router.get('/Services/Taxes', function(req, res){
-	res.render('services');
+// GET Taxes page 
+router.get('/Taxes', function(req, res){
+	res.render('taxes');
 });
 
-/* GET Payroll page */
-router.get('/Services/Payroll', function(req, res){
-	res.render('services');
+// GET Audit page 
+router.get('/Audit', function(req, res){
+	res.render('audit');
 });
-/*END OF DROPDOWN MENU */
+
+// GET Human Resources page 
+router.get('/HR', function(req, res){
+	res.render('HR');
+});
+
+// GET Software Developing page 
+router.get('/Software', function(req, res){
+	res.render('software');
+});
+/* END OF SERVICES */
+
 module.exports = router;
